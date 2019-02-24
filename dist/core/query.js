14aa45fa092f8224551f8c783a71ad69313ab1c4
Henry Harder <henry@paradigm.market>
2019-02-24 16:53:17 -0500

    workable ABCI query method

--- dist/core/query.js
+++ dist/core/query.js
@@ -2,14 +2,32 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function queryWrapper(state) {
     return (request) => {
-        let code, log, info, index, key, value, proof, height, codespace;
+        const height = state.lastBlockHeight;
+        let code, log, info, index, key, value, proof;
         const { data, path, height: reqHeight, prove, } = request;
-        return {
-            code: 0,
-            info: "Successful query.",
-            key: Buffer.from("lastBlockHeight"),
-            value: Buffer.from(state.lastBlockHeight.toString())
-        };
+        const pathTest = /^(posters|validators){1}\/(0x)[a-zA-Z0-9]{40}\/.*$/;
+        if (!pathTest.test(path)) {
+            code = 1;
+            log = "Failed query: invalid query path.";
+            info = "Currently query only accepts poster and validator queries.";
+        }
+        else {
+            const [subject, account, filter] = path.split("/");
+            info = path;
+            const accountObject = state[subject][account];
+            if (accountObject && accountObject[filter]) {
+                code = 0;
+                log = "Successful query.";
+                const result = accountObject[filter].toString();
+                key = Buffer.from(filter);
+                value = Buffer.from(result);
+            }
+            else {
+                code = 1;
+                log = "Failed query: invalid account or filter.";
+            }
+        }
+        return { code, info, log, key, value };
     };
 }
 exports.queryWrapper = queryWrapper;
--- src/core/query.ts
+++ src/core/query.ts
@@ -18,6 +18,9 @@ import { ResponseQuery } from "../typings/abci";
 /**
  * Return information about the state and software.
  * 
+ * THe `request.path` field is used to direct a query towards a particular state
+ * object. Currently, query requests can return information about 
+ * 
  * @todo parse path, support multiple query paths and options, util functions 
  * for buffering req/res objects
  *
@@ -25,23 +28,61 @@ import { ResponseQuery } from "../typings/abci";
  */
 export function queryWrapper(state: State): (r) => ResponseQuery {
     return (request) => {
+        // the height at which the query is checked
+        const height = state.lastBlockHeight;
+
         // return values (all possible) for `ResponseQuery`
-        let code, log, info, index, key, value, proof, height, codespace;
+        let code, log, info, index, key, value, proof;
 
         // destructure query request params
-        const {
-            data,
-            path,
-            height: reqHeight,
-            prove,
-        } = request;
+        // @todo leverage more of the possible RequestQuery params
+        const { path } = request;
 
-        // temporarily return minimum viable query response
-        return {
-            code: 0,
-            info: "Successful query.",
-            key: Buffer.from("lastBlockHeight"),
-            value: Buffer.from(state.lastBlockHeight.toString())
-        };
+        // path tester regex
+        // @todo improve and move
+        const pathTest = /^(posters|validators){1}\/(0x)[a-zA-Z0-9]{40}\/.*$/;
+
+        if (!pathTest.test(path)) {
+            code = 1;
+            log = "Failed query: invalid query path."
+            info = "Currently query only accepts poster and validator queries."
+
+            // construct ResponseQuery object
+            return { code, log, info, height }
+        }
+
+        // split path
+        const [ subject, account, filter] = path.split("/");
+
+        // check state object for fields 
+        const accountObject = state[subject][account];
+
+        // check for account and filter
+        if (accountObject && accountObject[filter]) {
+            // encode result as string
+            const result = accountObject[filter].toString();
+
+            // set success status
+            code = 0;
+            log = "Successful query.";
+
+            // buffer key and value for tags
+            key = Buffer.from(filter);
+            value = Buffer.from(result);
+
+            // include string version of result
+            info = result;
+
+            // construct ResponseQuery object
+            return { code, info, log, key, value, height};
+        }
+
+        // case for invalid request/missing account
+        code = 1;
+        log = "Failed query: invalid account or filter.";
+        info = path;
+
+        // construct ResponseQuery object
+        return { code, info, log, height };
     }
 }
